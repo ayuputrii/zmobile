cce7fb5278052488ffca4946d71105c4ee40318d
ayuputrii <ayuarmadani.pm@gmail.com>
2020-12-13 06:05:04 +0700

    midtrans

--- src/models/topup.js
+++ src/models/topup.js
@@ -1,59 +1,88 @@
-const db = require('../config/mysql')
+const db = require("../config/mysql");
+const services = require("../services");
 
 module.exports = {
-    getAllTopUp: function() {
-        return new Promise((resolve, reject) => {
-            db.query('SELECT * FROM topup ORDER BY sequence ASC', (err, result) => {
-                if(!err) {
-                    resolve(result)
-                } else {
-                    reject(new Error(err))
-                }
-            })
+  getAllTopUp: function () {
+    return new Promise((resolve, reject) => {
+      db.query("SELECT * FROM topup ORDER BY sequence ASC", (err, result) => {
+        if (!err) {
+          resolve(result);
+        } else {
+          reject(new Error(err));
+        }
+      });
+    });
+  },
+  getTopUpByOrder: function (order) {
+    return new Promise((resolve, reject) => {
+      db.query(`SELECT * FROM topup WHERE sequence=${order}`, (err, result) => {
+        if (!err) {
+          resolve(result);
+        } else {
+          reject(new Error(err));
+        }
+      });
+    });
+  },
+  postTopUp: function (setData) {
+    return new Promise((resolve, reject) => {
+      db.query(`INSERT INTO topup SET ?`, setData, (err, result) => {
+        if (!err) {
+          resolve(result);
+        } else {
+          reject(new Error(err));
+        }
+      });
+    });
+  },
+  addTransactionsData: function (data) {
+    return new Promise((resolve, reject) => {
+      db.query("INSERT INTO transaction SET ?", data, (err, result) => {
+        if (!err) {
+          resolve(result);
+        } else {
+          reject(new Error(err));
+        }
+      });
+    });
+  },
+  charge: function (amount, data) {
+    const { id } = req.token;
+    return new Promise((resolve, reject) => {
+      services
+        .postCharge(amount, data)
+        .then((results) => {
+          resolve(results);
         })
-    },
-    getTopUpByOrder: function(order) {
-        return new Promise((resolve, reject) => {
-            db.query(`SELECT * FROM topup WHERE sequence=${order}`, (err, result) => {
-                if(!err) {
-                    resolve(result)
-                } else {
-                    reject(new Error(err))
-                }
-            })
-        })
-    },
-    postTopUp: function(setData) {
-        return new Promise((resolve, reject) => {
-            db.query(`INSERT INTO topup SET ?`, setData, (err, result) => {
-                if(!err) {
-                    resolve(result)
-                } else {
-                    reject(new Error(err))
-                }
-            })
-        })
-    },
-    ediTopUp: function(order, setData) {
-        return new Promise((resolve, reject) => {
-            db.query('UPDATE topup SET ? WHERE sequence=?', [setData, order], (err, result) => {
-                if(!err) {
-                    resolve(result)
-                } else {
-                    reject(new Error(err))
-                }
-            })
-        })
-    },
-    deleteTopUp: function(order) {
-        return new Promise((resolve, reject) => {
-            db.query(`DELETE FROM topup WHERE sequence=${order}`, (err, result) => {
-                if(!err) {
-                    resolve(result)
-                } else {
-                    reject(new Error(err))
-                }
-            })
-        })
-    }
-}
+        .catch((err) => {
+          reject(new Error(err));
+        });
+    });
+  },
+  ediTopUp: function (order, setData) {
+    return new Promise((resolve, reject) => {
+      db.query(
+        "UPDATE topup SET ? WHERE sequence=?",
+        [setData, order],
+        (err, result) => {
+          if (!err) {
+            resolve(result);
+          } else {
+            reject(new Error(err));
+          }
+        }
+      );
+    });
+  },
+  deleteTopUp: function (order) {
+    return new Promise((resolve, reject) => {
+      db.query(`DELETE FROM topup WHERE sequence=${order}`, (err, result) => {
+        if (!err) {
+          resolve(result);
+        } else {
+          reject(new Error(err));
+        }
+      });
+    });
+  },
+};
--- src/routes/topup.js
+++ src/routes/topup.js
@@ -1,12 +1,23 @@
-const topupController = require('../controllers/topup')
-const router = require('express').Router()
-const { authentication , authorization } = require('../middlewares/auth')
+const topupController = require("../controllers/topup");
+const router = require("express").Router();
+const { authentication, authorization } = require("../middlewares/auth");
 
 router
-    .get('/', authentication, topupController.getAllTopUp)
-    .get('/:order', authentication, authorization, topupController.getTopUpByOrder)
-    .post('/', authentication, authorization, topupController.postTopUp)
-    .patch('/:order', authentication, authorization, topupController.editTopUp)
-    .delete('/:order', authentication, authorization, topupController.deleteTopUp)
+  .get("/", authentication, topupController.getAllTopUp)
+  .get(
+    "/:order",
+    authentication,
+    authorization,
+    topupController.getTopUpByOrder
+  )
+  .post("/", authentication, authorization, topupController.postTopUp)
+  .post("/charge", authentication, topupController.chargeTopUp)
+  .patch("/:order", authentication, authorization, topupController.editTopUp)
+  .delete(
+    "/:order",
+    authentication,
+    authorization,
+    topupController.deleteTopUp
+  );
 
-module.exports = router
+module.exports = router;
